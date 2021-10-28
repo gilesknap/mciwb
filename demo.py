2a990326c3253461e257aee4ae60a5a3b9d2a21d
Giles Knap <gilesknap@gmail.com>
2021-10-28 07:23:48 +0100

    pare down demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,5 @@
 import sys
 from getpass import getpass
-from time import sleep
-from typing import cast
 
 from mcipc.rcon.item import Item
 from mcipc.rcon.je import Client
@@ -10,36 +8,6 @@ from mcwb.types import Planes3d
 
 from mciwb.copy import Copy
 from mciwb.player import Player
-
-useful_classes = [Player]
-
-
-
-# my server ports
-science = 20501
-
-
-def setup(client):
-    # don't announce every rcon command
-    client.gamerule("sendCommandFeedback", False)
-
-
-def funky_cube(size: int) -> Cuboid:
-    half = int(size / 2)
-    size = 2 * half
-
-    left = [Item.RED_CONCRETE] * size
-    right = [Item.GREEN_CONCRETE] * size
-    row = [Item.BLUE_CONCRETE] + [Item.AIR] * (size - 2) + [Item.YELLOW_CONCRETE]
-    square = [left] + [row] * (size - 2) + [right]
-
-    top = [
-        [Item.WHITE_CONCRETE, Item.GRAY_CONCRETE] * half,
-        [Item.GRAY_CONCRETE, Item.WHITE_CONCRETE] * half,
-    ] * half
-    bottom = [[Item.BLACK_CONCRETE, Item.GRAY_CONCRETE] * half] * size
-
-    return cast(Cuboid, [top] + [square] * (size - 2) + [bottom])
 
 
 c: Client
@@ -56,11 +24,12 @@ def bye():
 
 
 def connect():
-    if len(sys.argv) > 1:
-        passw = sys.argv[1]
+    port = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        passw = sys.argv[2]
     else:
-        passw = getpass(f"Password for mc server at port {science}: ")
-    c = Client("localhost", science, passwd=passw)
+        passw = getpass(f"Password for localhost mc server at rcon port {port}: ")
+    c = Client("localhost", port, passwd=passw)
     c.connect(True)
     print("connected")
     # don't announce every rcon command
@@ -73,12 +42,3 @@ if __name__ == "__main__":
     c = connect()
     p = Player(c, "TransformerScorn")
     cp = Copy(c, "TransformerScorn")
-
-    pos = Vec3(0, 150, -40)
-    cube = funky_cube(30)
-    anchor = Anchor3.BOTTOM_MIDDLE
-
-    fun_cube = Blocks(c, pos, cube, anchor=anchor)
-
-    plane = Planes3d.XZ
-    fun_cube.rotate(plane)
