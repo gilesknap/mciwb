904cec91dbe8590b6b31789f00ab26f34e76b5b8
Giles Knap <gilesknap@gmail.com>
2022-06-20 21:37:42 +0100

    get test players feet on the ground

--- tests/conftest.py
+++ tests/conftest.py
@@ -167,7 +167,7 @@ def minecraft_player(minecraft_client):
 
     # summon a fixed position, named entity as a substitute for a player
     res = minecraft_client.summon(
-        "armor_stand", Vec3(0, 4, 0), {"CustomName": f'"{ENTITY_NAME}"'}
+        "armor_stand", Vec3(0, 3, 0), {"CustomName": f'"{ENTITY_NAME}"'}
     )
     print(res)
 
