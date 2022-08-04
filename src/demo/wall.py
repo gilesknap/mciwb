bf6a258bfa4070439d4d7527aeeecf5963c03319
Giles Knap <giles.knap@gmail.com>
2022-08-04 10:33:35 +0100

    wall function impovements

--- src/demo/wall.py
+++ src/demo/wall.py
@@ -3,22 +3,28 @@ import numpy as np
 from mciwb.imports import Direction, Item, get_client
 
 
-def wall(start, end):
-    c = get_client()
-    dx = end.x - start.x
-    dz = end.z - start.z
+def wall(start, stop):
+    def section(begin, wall_dir, step_dir):
+        end = begin + wall_dir
+        c.fill(begin.with_ints(), end.with_ints() + Direction.UP * 5, Item.STONE)
+        begin = end + step_dir
+        return begin, end
 
-    section_begin = start
+    c = get_client()
+    dx = stop.x - start.x
+    dz = stop.z - start.z
+
+    begin = start
 
     if abs(dx) > abs(dz):
-        z_dir = np.sign(dz)
         count = abs(dz + 1)
+        wall_dir = Direction.EAST * dx / count
 
         for step in range(count):
-            section_end = section_begin + Direction.EAST * dx / count
-            c.fill(
-                section_begin.with_ints(),
-                section_end.with_ints() + Direction.UP * 5,
-                Item.STONE,
-            )
-            section_begin = section_end + Direction.SOUTH * z_dir
+            begin, end = section(begin, wall_dir, Direction.SOUTH * np.sign(dz))
+    else:
+        count = abs(dx + 1)
+        wall_dir = Direction.SOUTH * dz / count
+
+        for step in range(count):
+            begin, end = section(begin, wall_dir, Direction.EAST * np.sign(dx))
