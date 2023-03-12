ecce642d88e3ce1881e4592d377c2dd96cb72176
gilesknap <gilesknap@users.noreply.github.com>
2023-03-12 09:35:54 +0000

    deploy: ef7f3c210778cbb935122ce1b7a59a8f9e8f4d19

--- skeleton-merge/_static/documentation_options.js
+++ skeleton-merge/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.5.dev36+gdaf4975',
+    VERSION: '0.8.5.dev36+gef7f3c2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
