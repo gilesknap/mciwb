c2591c64c3e30895cf6357797bc1215a4d4ef4eb
gilesknap <gilesknap@users.noreply.github.com>
2023-03-12 08:24:13 +0000

    deploy: eaca2b230a3b6c24d4488486583a4f77268afc76

--- pypi-fix/_static/documentation_options.js
+++ pypi-fix/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.4.dev1+gfe6bc7f',
+    VERSION: '0.8.4.dev2+geaca2b2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
