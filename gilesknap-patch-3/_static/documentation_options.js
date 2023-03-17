6303fddd64ad97a96cf93a4fb3438f45fbe0ebee
gilesknap <gilesknap@users.noreply.github.com>
2023-03-17 21:06:15 +0000

    deploy: 273313f634df3b8d452dbbbd1b0842e187e923f2

--- gilesknap-patch-3/_static/documentation_options.js
+++ gilesknap-patch-3/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.6.dev9+g541b266',
+    VERSION: '0.8.6.dev11+g273313f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
